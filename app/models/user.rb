649e3213b725c82837b136f7eb9cc8465a04e79e
Marcin Felczak <feleck@gmail.com>
2015-04-10 21:29:27 +0200

    user model fixed, specs green

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  
 
   has_many :reviews
   has_many :products
