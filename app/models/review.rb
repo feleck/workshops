903e386b9f29558dde3aa667e4cfccba39841aac
Marcin Felczak <feleck@gmail.com>
2015-04-11 11:04:04 +0200

    review model fixed, specs green

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :rating , presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true 
+  #test pasess, but according to http://guides.rubyonrails.org/active_record_validations.html#validates-associated should be:
+  # valideates :user, presence: true
 end
