4d8235db51f0dbc8bc62dd3ed638d4c57122390f
Marcin Felczak <feleck@gmail.com>
2015-04-11 10:53:26 +0200

    product model fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, format: { with: /\A\d+.?\d{,2}?\z/ }
+
+  def average_rating
+    ratings = self.reviews.all.collect { |review| review.rating }
+    ratings.inject { |sum, rating| sum + rating }.to_f / ratings.size
+  end
+
 end
