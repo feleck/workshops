1833e6e9304ef8c3e831efb0fb1f34eb9249436a
Marcin Felczak <feleck@gmail.com>
2015-04-19 01:44:47 +0200

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,51 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-user1 = User.create(
-  email: "test1@email.com",
-  password: "pass1234!",
-  password_confirmation: "pass1234!",
-  firstname: "Jan",
-  lastname: "Smith")
+#require 'faker'
 
-admin = User.create(
-  email: "admin1@email.com",
-  password: "pass1234!",
-  password_confirmation: "pass1234!",
-  firstname: "John",
-  lastname: "Kowalski")
+5.times do |i|
+  User.create(email: "test#{i}@email.com",
+              password: "pass1234!",
+              password_confirmation: "pass1234!",
+              firstname: Faker::Name.first_name,
+              lastname: Faker::Name.last_name)
+end
 
-admin.toggle!(:admin)
+User.first.update_attribute(:admin, true)
+
+Category.create(name: 'Toys')
+Category.create(name: 'Books')
+
+5.times do |i| # Count of products
+  Product.create(title: "Book ##{i}", 
+                  description: "#{i} product description",
+                  price: rand(1..500),
+                  category_id: rand(1..2), 
+                  user_id: rand(1..5))
+end
+
+Product.create(
+  title: 'ball',
+  description: 'big beach ball',
+  price: 4.53,
+  category_id: 1,
+  user_id: 2)
+
+Product.create(
+  title: 'Admin wand',
+  description: 'Magic wand for admin!',
+  price: 12.38,
+  category_id: 1,
+  user_id: 1)
+
+15.times do |i| # Count of reviews
+  Review.create(content: "Review number #{i} - content.", 
+                rating: rand(1..5),
+                product_id: rand(1..7),
+                user_id: rand(1..5))
+end
+
+Review.create(content: "Other review content.", 
+              rating: 4,
+              product_id: 1,
+              user_id: 3)
