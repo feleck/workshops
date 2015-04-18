d3e9acbf6fa09d69efd8ebc9ea8e5791e49ae399
Marcin Felczak <feleck@gmail.com>
2015-04-18 16:08:45 +0200

    firstname&lastname added to application controller - devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :devise_controller?
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protect_from_forgery with: :exception
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:firstname, :lastname]
+  end
 end
