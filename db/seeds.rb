8a58370fa59de61df5ea37bb2cccb78b2df7c401
Marcin Felczak <feleck@gmail.com>
2015-04-18 15:14:47 +0200

    seeds user and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create(
+  email: "test1@email.com",
+  password: "pass1234!",
+  password_confirmation: "pass1234!",
+  firstname: "Jan",
+  lastname: "Smith")
+
+admin = User.create(
+  email: "admin1@email.com",
+  password: "pass1234!",
+  password_confirmation: "pass1234!",
+  firstname: "John",
+  lastname: "Kowalski")
+
+admin.toggle!(:admin)
